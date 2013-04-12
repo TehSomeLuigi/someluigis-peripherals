b0982a045067bcb0a547dbb5375f76656471a1ed
TehSomeLuigi <someluigi@rocketmail.com>
2013-04-12 16:18:16 +0100

    Passed cookies to request, though for now you'll have to set cookies with javascript.

--- slp_common/com/someluigi/slperiph/server/SLPHTTPServer.java
+++ slp_common/com/someluigi/slperiph/server/SLPHTTPServer.java
@@ -4,8 +4,13 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.WeakHashMap;
 
+import net.minecraft.util.EnumChatFormatting;
+
+import org.simpleframework.http.Cookie;
 import org.simpleframework.http.Path;
 import org.simpleframework.http.Query;
 import org.simpleframework.http.Request;
@@ -16,6 +21,7 @@ import org.simpleframework.transport.Server;
 import org.simpleframework.transport.connect.Connection;
 import org.simpleframework.transport.connect.SocketConnection;
 
+import com.someluigi.slperiph.SLPMod;
 import com.someluigi.slperiph.tileentity.TileEntityHTTPD;
 
 import dan200.computer.api.IComputerAccess;
@@ -41,6 +47,9 @@ public class SLPHTTPServer implements Container {
             System.err
                     .println("Caught exception from SLP HTTP Server in START");
             e.printStackTrace();
+            
+            SLPMod.httpdStat = EnumChatFormatting.RED + "ERRORED - " + e.getClass().getName() + ": " + e.getMessage();
+            
         }
 
         // connection.close();
@@ -71,7 +80,7 @@ public class SLPHTTPServer implements Container {
             }
 
             res.setValue("Content-Type", "text/html");
-            res.setValue("Server", "HelloWorld/1.0 (Simple 4.0)");
+            res.setValue("Server", "SLPeripherals/1.0 (Simple 4.0)");
             res.setDate("Date", time);
             res.setDate("Last-Modified", time);
 
@@ -94,9 +103,16 @@ public class SLPHTTPServer implements Container {
 
                         // reqs.add("{bl}");
                         // int idx=reqs.lastIndexOf("{bl}");
-
+                        
+                        
+                        Map<String, String> cookies = new HashMap<String, String>();
+                        
+                        for (Cookie c: res.getCookies()) {
+                            cookies.put(c.getName(), c.getValue());
+                        }
+                        
                         ica.queueEvent("http_server_request", new Object[] {
-                                idx, p.getPath(1), qry });
+                                idx, p.getPath(1), qry, cookies });
 
                         return;
 
@@ -127,13 +143,11 @@ public class SLPHTTPServer implements Container {
 
                 }
             } catch (Exception e) {
-                // if (url.equals("/")) {
                 ps.print("SLP HTTP Server (based on the Simple Framework)<br>"
                         + "Please go to a service by typing the computer id then a slash, example:<br>"
                         + "http://localhost/4/<br>"
                         + "would go to Computer ID 4's service. - The IDs are assigned by ComputerCraft.<br>"
-                        + "(please note 'localhost' depends on your setup. You may have to use a port number too. See the current URL.)");
-                // }
+                        + "(please note 'localhost' depends on your setup. You may have to use a port number too. See the current URL.).<br>(exception encountered)");
             }
 
             ps.close();
